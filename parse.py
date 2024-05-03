67d70531350a31d388d169a874e993faa18ba813
Anmol Sharma <keepupsharma@gmail.com>
2024-05-03 14:04:50 -0500

    improve tracking

--- parse.py
+++ parse.py
@@ -1,19 +1,19 @@
-# %%
-# !pip install --upgrade google-cloud-storage google-cloud-bigquery google-cloud-bigquery-storage
-# !gcloud auth application-default login
 from google.cloud import storage
 import re
 import pprint
 from urllib.parse import parse_qs
 import psycopg2
 import os
+import json
+from geoip import geolite2
+import datetime
+
+
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.abspath('')+'/key.json'
 
-# %%
 storage_client = storage.Client()
 
 
-# %%
 def list_blobs():
     bucket_name = "pixel-logs-outreach-genius"
 
@@ -22,85 +22,142 @@ def list_blobs():
 
     return blobs
 
-# %%
-urls = []
+
+parsed_urls = []
 
 for blob in list_blobs():
-    downloaded_blobs = blob.download_as_string().decode('utf-8')
-    urls = urls + [parse_qs(x) for x in (re.findall(r'"requestUrl":"https://pixel.outreachgenius.ai/pixel.gif\?([^"]*)"',downloaded_blobs))]
-    blob.delete()
+    downloaded_blobs = blob.download_as_string().decode("utf-8").split("\n")
+    for item in downloaded_blobs:
+        if len(item) > 2:
+            parsed_blob = json.loads(item)
+            parsed_url = parse_qs(
+                parsed_blob["httpRequest"]["requestUrl"].split("?")[1]
+            )
+            parsed_url["ip"] = geolite2.lookup(parsed_blob["httpRequest"]["remoteIp"])
+            parsed_url["latency"] = parsed_blob["httpRequest"]["latency"]
+            parsed_urls.append(parsed_url)
 
-pprint.pp(urls)
+pprint.pp(parsed_urls[0])
 
-# %%
-conn= psycopg2.connect(
+conn = psycopg2.connect(
     database="postgres",
     user="postgres",
     password="postgres",
     host="pixel-log.cxgy2o6qilhd.us-west-2.rds.amazonaws.com",
-    port='5432'
+    port="5432",
 )
 
-import datetime
-
 eventmap = {
-"ctaClick":"cta_clicks",
-"formInteraction":"form_interactons",
-"videoEngagement":"video_engagements",
-"socialMediaInteraction":"social_media_clicks",
-"download":"file_downloads",
-"productReview":"product_reviews",
-"emailSubscribe":"email_subscriptions"
+    "ctaClick": "cta_clicks",
+    "formInteraction": "form_interactons",
+    "videoEngagement": "video_engagements",
+    "socialMediaInteraction": "social_media_clicks",
+    "download": "file_downloads",
+    "productReview": "product_reviews",
+    "emailSubscribe": "email_subscriptions",
 }
+
 
 def sanitize_input(inputData):
     # Use regular expression to remove non-alphanumeric characters
-    sanitized_data = re.sub(r'[^a-zA-Z0-9\-\_\s]', '', inputData)
+    sanitized_data = re.sub(r"[^a-zA-Z0-9\-\_\s]", "", inputData)
     return sanitized_data
 
-def createColumn(sessionId, timestamp, pid, uid,url,location,referer):
-    query = 'INSERT INTO Logs (sessionId, visited_at, pid, uid,url,location,referer) VALUES (%s, %s, %s, %s, %s,%s,%s)'
+
+def createColumn(
+    sessionId,
+    timestamp,
+    pid,
+    uid,
+    url,
+    referer,
+    screen_width,
+    screen_height,
+    country,
+    timezone,
+    subdivisions,
+    user_agent,
+    latency,
+    ip_address,
+    browser,
+    is_mobile,
+):
+    query = "INSERT INTO Logs (sessionId, visited_at, pid, uid,url,referer,screen_width,screen_height,country,timezone,subdivisions,user_agent,latency,ip_address,browser,is_mobile) VALUES (%s, %s, %s, %s, %s, %s,%s,%s,%s, %s,%s,%s,%s,%s,%s,%s)"
     cur = conn.cursor()
-    cur.execute(query, (sessionId, timestamp, pid, uid,url,location,referer))
-    print('created column')
+    cur.execute(
+        query,
+        (
+            sessionId,
+            timestamp,
+            pid,
+            uid,
+            url,
+            referer,
+            screen_width,
+            screen_height,
+            country,
+            timezone,
+            subdivisions,
+            user_agent,
+            latency,
+            ip_address,
+            browser,
+            is_mobile,
+        ),
+    )
+    print("created column")
     conn.commit()
 
+
 def logEvent(sessionId, event):
-    query = f'UPDATE Logs SET {event} = {event} + 1 WHERE sessionId = %s'
+    query = f"UPDATE Logs SET {event} = {event} + 1 WHERE sessionId = %s"
     cur = conn.cursor()
     cur.execute(query, (sessionId,))
     conn.commit()
 
+
 def setValue(sessionId, column, value):
-    query = f'UPDATE Logs SET {column} = %s WHERE sessionId = %s'
+    query = f"UPDATE Logs SET {column} = %s WHERE sessionId = %s"
     cur = conn.cursor()
     cur.execute(query, (value, sessionId))
     conn.commit()
 
+
 def handleData(data):
-    if (data["ev"][0] == 'pageView') :
-        createColumn(data['sessionId'][0],datetime.datetime.fromtimestamp(int(data['ts'][0])/1000),data['pid'][0],data['uid'][0],data['dl'][0],'TODO','TODO')
-        
-    if (data["ev"][0] == 'sessionEnd') :
-        setValue(data['sessionId'][0],'session_duration',eval(data['ed'][0])['timeSpent'])
+    if data["ev"][0] == "pageView":
+        createColumn(
+            data["sessionId"][0],
+            datetime.datetime.fromtimestamp(int(data["ts"][0]) / 1000),
+            data["pid"][0],
+            data["uid"][0],
+            data["dl"][0],
+            "TODO",
+            int(data["sr"][0].split("x")[0]),
+            int(data["sr"][0].split("x")[1]),
+            data["ip"].country,
+            data["ip"].timezone,
+            ",".join(list(data["ip"].subdivisions)),
+            data["ua"][0],
+            data["latency"],
+            data["ip"].ip,
+            data["bn"][0],
+            data["md"][0],
+        )
+
+    if data["ev"][0] == "sessionEnd":
+        setValue(
+            data["sessionId"][0], "session_duration", eval(data["ed"][0])["timeSpent"]
+        )
     try:
-        logEvent(data['sessionId'][0],eventmap[data['ev'][0]])
+        logEvent(data["sessionId"][0], eventmap[data["ev"][0]])
     except Exception as e:
         print(e)
 
 
-# %%
-for data in urls:
+for data in parsed_urls:
     try:
         handleData(data)
-    except Exception:
-        print(Exception)
-conn.close()
-
-# %%
-
-
-# %%
-
-
-
+    except Exception as e:
+        conn.close()
+        print(e, e.args)
+        break
--- setupdb.py
+++ setupdb.py
@@ -12,26 +12,35 @@ db= psycopg2.connect(
 cur = db.cursor()
 
 cur.execute('''
-            CREATE Table Logs (
-uid varchar(200),
-pid varchar(200),
-sessionId varchar(200),
-visited_at timestamp,
-session_duration int default 0,
-cta_clicks int default 0,
-form_interactons int default 0,
-scroll_depth float default 0,
-referer varchar(400),
-url varchar(400),
-ip varchar(100),
-video_engagements int default 0,
-social_media_clicks int default 0,
-file_downloads int default 0,
-product_reviews int default 0,
-email_subscriptions int default 0
+DROP TABLE IF EXISTS Logs;
+CREATE Table Logs (
+    uid varchar(200),
+    pid varchar(200),
+    sessionId varchar(200),
+    visited_at timestamp,
+    session_duration int default 0,
+    cta_clicks int default 0,
+    form_interactons int default 0,
+    scroll_depth float default 0,
+    referer varchar(400),
+    url varchar(400),
+    screen_width int,
+    screen_height int,
+    latency varchar(100),
+    user_agent varchar(200),
+    country varchar(200),
+    subdivisions varchar(200),
+    timezone varchar(200),
+    video_engagements int default 0,
+    social_media_clicks int default 0,
+    file_downloads int default 0,
+    product_reviews int default 0,
+    email_subscriptions int default 0,
+    browser varchar(100),
+    is_mobile bool,
+    ip_address varchar(100)
 );
-            
-            ''')
+''')
 
 db.commit()
 
